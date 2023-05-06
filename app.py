56eb9673338f89ebaf2ec5d05c8ad65b6acb3fae
Yuki <82790806+Taaaa-yuki@users.noreply.github.com>
2023-05-06 10:33:57 +0900

    Improved error handling

--- app.py
+++ app.py
@@ -1,44 +1,52 @@
 import re
-from bs4 import BeautifulSoup
-from flask import Flask, jsonify, request
-import requests
 import traceback
-
+from bs4 import BeautifulSoup
+import requests
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
-@app.route("/scrape", methods=["POST", "GET"])
+@app.route('/scrape', methods=['POST'])
 def scrape():
-    if request.method == "POST":
-        url = request.form.get("url")
-        try:
-            html = requests.get(url).text
-            soup = BeautifulSoup(html, "html.parser")
+    url = request.form.get('url')
+    if not url:
+        return jsonify({'error': 'Missing url parameter.'}), 400
 
-            title = soup.find("h2", class_="ms-2 ms-md-3").text.strip()
-            artist = soup.find("span", itemprop="byArtist name").text.strip()
-            body = soup.find("div", id="kashi_area").prettify()
-            body = re.sub(r"\s*<br\s*/?>\s*", "\n", body).strip()
-            body = BeautifulSoup(body, "html.parser").text.strip()
+    try:
+        html = requests.get(url).text
+        soup = BeautifulSoup(html, 'html.parser')
+
+        title = soup.find('h2', class_='ms-2 ms-md-3').text.strip()
+        artist = soup.find('span', itemprop='byArtist name').text.strip()
+        body = soup.find('div', id='kashi_area')
+        if not body:
+            raise ValueError('Failed to find kashi_area div.')
+        body = re.sub(r'\s*<br\s*/?>\s*', '\n', str(body)).strip()
+        body = BeautifulSoup(body, 'html.parser').text.strip()
+
+        result = {
+            'title': title,
+            'artist': artist,
+            'body': body
+        }
+
+        return jsonify(result)
+
+    except requests.exceptions.RequestException as e:
+        traceback.print_exc()
+        return jsonify({'error': str(e)}), 500
+
+    except ValueError as e:
+        traceback.print_exc()
+        return jsonify({'error': str(e)}), 500
 
 
-            result = {
-                "title": title,
-                "artist": artist,
-                "body": body
-            }
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return jsonify({'message': 'Welcome to the lyrics API.'})
 
-            return jsonify(result)
-        except:
-            # traceback.print_exc()
-            # return jsonify({"error": str(e)})
-            return jsonify({"error": "Failed to scrape body."})
-    elif request.method == "GET":
-        return "Please make a POST request to /scrape with the 'url' parameter."
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
